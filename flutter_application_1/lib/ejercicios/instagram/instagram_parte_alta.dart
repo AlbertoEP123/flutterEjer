7e6e6ff8676cdfdcb18e8a1e69c9c22ccbbd4067
AlbertoEP123 <aalbertoenriquepulido@gmail.com>
2024-11-06 11:54:54 +0100

    a

--- flutter_application_1/lib/ejercicios/instagram/instagram_parte_alta.dart
+++ flutter_application_1/lib/ejercicios/instagram/instagram_parte_alta.dart
@@ -11,44 +11,114 @@ class Instagrampartealta extends StatelessWidget {
       appBar: AppBar(),
       body: Column(
         children: [
+          // Container nombre usuario
           Container(
-            padding: const EdgeInsets.all(16),
-            color: const Color.fromARGB(25, 25, 25, 25),
+            padding: const EdgeInsets.only(top: 20.0, left: 20.0),
+            color: const Color.fromARGB(255, 255, 255, 255),
             child: const Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Column(children: [
-                  Text("alvarezdelvayo "),
-                  CircleAvatar(radius: 40),
-                ]),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  
-                  children: [
-                    SizedBox(width: 65),
-                    Text("1022 \nPublicaciones",),
-                    SizedBox(width: 25),
-                    Text("859 \nSeguidores"),
-                    SizedBox(width: 25),
-                    Text("211 \nSeguidos"),
-                  ],
+                Text(
+                  "alvarezdelvayo",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                  ),
                 ),
               ],
-            
             ),
-           
-            
           ),
-           const Padding(
-              padding: EdgeInsets.only(left: 100),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(padding: EdgeInsets.all(3)),
-                  
-                ],
-              ),
+          // Container avatar y publicaciones..
+          Container(
+            padding: const EdgeInsets.all(30),
+            color: const Color.fromARGB(255, 255, 255, 255),
+            child: Row(
+              children: [
+                const CircleAvatar(
+                  radius: 40,
+                  backgroundImage: AssetImage(''),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            '1026\nPublicaciones',
+                            style: TextStyle(fontSize: 12),
+                          ),
+                          SizedBox(width: 1),
+                          Text(
+                            '859\nSeguidores',
+                            style: TextStyle(fontSize: 12),
+                          ),
+                          SizedBox(width: 1),
+                          Text(
+                            '211\nSeguidos',
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      // Container biografia
+                      Container(
+                        color: const Color.fromARGB(255, 255, 255, 255),
+                        child: const Column(
+                          children: [
+                            Text(
+                              "Fernando Álvarez del Vayo\nNunca sabes lo que te depara el futuro",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            ),
+                           
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        // editar perfil
+                        padding: const EdgeInsets.only(left: 50),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(3),
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 8, vertical: 3),
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: const Color.fromARGB(
+                                        255, 148, 148, 148),
+                                  ),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                width: 240,
+                                height: 28,
+                                alignment: Alignment.center,
+                                child: const Text(
+                                  'Editar perfil',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 13,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
+          ),
         ],
       ),
     );
